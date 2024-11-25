bbddb010a257e757571ea27d1b852ccc387afa5f
nckugs <yclin@gs.nfu.edu.tw>
2024-11-25 14:08:53 +0800

    Combine with TcpFileSender class!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "tcpfileserver.h"
+#include "tcpfilesender.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    TcpFileServer w;
-    w.show();
+    TcpFileServer w1;
+    TcpFileSender w2;
+    w1.show();
+    w2.show();
 
     return a.exec();
 }
